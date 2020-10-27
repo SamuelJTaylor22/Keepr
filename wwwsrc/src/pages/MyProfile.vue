276d61cd73c247f4f0e5b6acefcd1f2edf729613
SamuelJTaylor22 <sjtaylor1998@gmail.com>
2020-10-27 11:16:18 -0600

    added routes

---
+++ wwwsrc/src/pages/MyProfile.vue
@@ -0,0 +1,13 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+name:"myprofile"
+}
+</script>
+
+<style>
+
+</style>
--- wwwsrc/src/router/index.js
+++ wwwsrc/src/router/index.js
@@ -2,6 +2,13 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 // @ts-ignore
 import Home from "../pages/Home.vue";
+// @ts-ignore
+import MyProfile from "../pages/MyProfile.vue";
+// @ts-ignore
+import Profile from "../pages/Profile.vue";
+// @ts-ignore
+import Vault from "../pages/Vault.vue";
+import { authGuard } from "@bcwdev/auth0-vue";
 
 Vue.use(VueRouter);
 
@@ -10,6 +17,22 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home,
+  },
+  {
+    path: "/profile",
+    name: "MyProfile",
+    component: MyProfile,
+    beforeEnter: authGuard
+  },
+  {
+    path:"/profile/:id",
+    name:"Profile",
+    component: Profile
+  },
+  {
+    path:"vault/:id",
+    name:"Vault",
+    component: Vault
   }
 ];
 
