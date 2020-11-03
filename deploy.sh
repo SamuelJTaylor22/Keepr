d3598fa2492a24bd828d3b344cdf28053b3136d2
SamuelJTaylor22 <sjtaylor1998@gmail.com>
2020-11-03 14:29:29 -0700

    added names to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 dotnet publish -c Release
-docker build -t TAG_NAME ./bin/Release/netcoreapp3.1/publish
-docker tag TAG_NAME registry.heroku.com/HEROKU_APP_NAME/web
-docker push registry.heroku.com/HEROKU_APP_NAME/web
-heroku container:release web -a HEROKU_APP_NAME
+docker build -t keepr ./bin/Release/netcoreapp3.1/publish
+docker tag keepr registry.heroku.com/sjtkeepr/web
+docker push registry.heroku.com/sjtkeepr/web
+heroku container:release web -a sjtkeepr
